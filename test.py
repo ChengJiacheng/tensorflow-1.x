9d4c3e96cd198447d95e31cfab203cabceb41f55
ChengJiacheng <1967436138q@gmail.com>
2022-05-10 18:14:15 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,139 @@
+# %%
+import os
+import tensorflow as tf
+import numpy as np
+
+# %%
+# https://mofanpy.com/tutorials/machine-learning/tensorflow/example2/
+
+
+
+print(tf.__version__)
+
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+# %%
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights*x_data + biases
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+# init = tf.initialize_all_variables() # tf 马上就要废弃这种写法
+init = tf.global_variables_initializer()  # 替换成这样就好
+
+sess = tf.Session()
+sess.run(init)          # Very important
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases))
+
+# %%
+# create two matrixes
+matrix1 = tf.constant([[3,3]])
+matrix2 = tf.constant([[2],
+                       [2]])
+product = tf.matmul(matrix1,matrix2)
+
+# method 1
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+# [[12]]
+
+# method 2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
+# [[12]]
+
+# %%
+state = tf.Variable(0, name='counter')
+
+# 定义常量 one
+one = tf.constant(1)
+
+# 定义加法步骤 (注: 此步并没有直接计算)
+new_value = tf.add(state, one)
+
+# 将 State 更新成 new_value
+update = tf.assign(state, new_value)
+
+# 如果定义 Variable, 就一定要 initialize
+# init = tf.initialize_all_variables() # tf 马上就要废弃这种写法
+init = tf.global_variables_initializer()  # 替换成这样就好
+ 
+# 使用 Session
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
+
+# %%
+
+#在 Tensorflow 中需要定义 placeholder 的 type ，一般为 float32 形式
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+
+# mul = multiply 是将input1和input2 做乘法运算，并输出为 output 
+ouput = tf.multiply(input1, input2)
+
+with tf.Session() as sess:
+    print(sess.run(ouput, feed_dict={input1: 7., input2: 2.}))
+# %%
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# Make up some real data
+x_data = np.linspace(-1,1,300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# the error between prediction and real data
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# important step
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
+if int((tf.__version__).split('.')[1]) < 12:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    # training
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        # to see the step improvement
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+# %%
